b5bf81c56b27af2bc80a62edc792d7d7a5377d4b
Lucas Falaschini <lucasfalaschini@gmail.com>
2020-01-17 21:11:24 -0300

    Prueba1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CarruselComponent } from './components/carrusel/carrusel.component';
+import { PaqueteComponent } from './components/paquete/paquete.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    CarruselComponent,
+    PaqueteComponent
   ],
   imports: [
     BrowserModule
