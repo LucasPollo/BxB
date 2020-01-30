6f3bc40a40d3080f79be2c627789fea5b9c4140c
Lucas Falaschini <lucasfalaschini@gmail.com>
2020-01-29 21:19:48 -0300

    Agregacion de info en cartas

--- src/app/components/paquete/paquete.component.ts
+++ src/app/components/paquete/paquete.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { NgForm } from '@angular/forms';
+
+import Swal from 'sweetalert2';
+
 @Component({
   selector: 'app-paquete',
   templateUrl: './paquete.component.html',
@@ -10,6 +14,23 @@ export class PaqueteComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  mostrarinfo(form: NgForm){
+    
+    // Swal.fire({
+    //   title: 'Prueba',
+    //   text: 'Demostracion de SweetAlert',
+    //   icon: 'info',
+    //   allowOutsideClick: false
+    // });
+    // Swal.showLoading();
+    Swal.fire({
+      icon: 'info',
+      title: 'SweetAlert',
+      text: 'Que te parece?'
+    })
+
   }
 
 }
